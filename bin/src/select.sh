32dbd7128b8dd5fb14f57f53e1384452f9c63410
Jennifer Denis <jennifer.brody.denis@gmail.com>
2019-03-22 18:40:57 +0100

    :sparkles: [Select] Add new icons

--- bin/src/select.sh
+++ bin/src/select.sh
@@ -10,7 +10,7 @@ source "${BASH_SOURCE%/*}/assets/colors.sh" || source assets/colors.sh
 
 function selectEmoji()
 {
-  echo -e "\n\n $COLOR_LIGHT_CYAN List of available Emojis  $COLOR_NONE\n"
+  echo -e "\n\n $COLOR_LIGHT_CYAN Available Emojis  $COLOR_NONE\n"
 
   COUNTER=0
   while [[  ${COUNTER} -lt 42 ]]; do
@@ -41,6 +41,12 @@ function selectEmoji()
     echo -e "\t 24 🏗 $COLOR_DARK_THIN_GREEN Making architectural changes$COLOR_NONE"
     echo -en "\t 25 🚀$COLOR_DARK_THIN_GREEN Deploying stuff$COLOR_NONE"
     echo -e "\t\t\t\t 26 📦$COLOR_DARK_THIN_GREEN Updating compiled files or packages$COLOR_NONE"
+    echo -en "\t 27 ➕$COLOR_DARK_THIN_GREEN Adding a dependency$COLOR_NONE"
+    echo -e "\t\t\t 28 ➖$COLOR_DARK_THIN_GREEN Removing a dependency$COLOR_NONE"
+    echo -e "\t 29 📄$COLOR_DARK_THIN_GREEN Adding or updating license$COLOR_NONE"
+    
+
+
 
     echo -e "\t x \033[0;2mNone$COLOR_NONE"
 
@@ -51,138 +57,123 @@ function selectEmoji()
       1 )
         selectedEmoji=':bug:'
         prettyEmoji='🐛'
-        return 0
-        ;;
+        return 0 ;;
       2 )
         selectedEmoji=':speech_balloon:'
         prettyEmoji='💬'
-        return 0
-        ;;
+        return 0 ;;
       3 )
         selectedEmoji=':sparkles:'
         prettyEmoji='✨'
-        return 0
-        ;;
+        return 0 ;;
       4 )
         selectedEmoji=':memo:'
         prettyEmoji='📝'
-        return 0
-        ;;
+        return 0 ;;
       5 )
         selectedEmoji=':lipstick:'
         prettyEmoji='💄'
-        return 0
-        ;;
+        return 0 ;;
       6 )
         selectedEmoji=':construction:'
         prettyEmoji='🚧'
-        return 0
-        ;;
+        return 0 ;;
       7 )
         selectedEmoji=':green_heart:'
         prettyEmoji='💚'
-        return 0
-        ;;
+        return 0 ;;
       8 )
         selectedEmoji=':art:'
         prettyEmoji='🎨'
-        return 0
-        ;;
+        return 0 ;;
       9 )
         selectedEmoji=':truck:'
         prettyEmoji='🚚'
-        return 0
-        ;;
+        return 0 ;;
       10 )
         selectedEmoji=':children_crossing:'
         prettyEmoji='🚸'
-        return 0
-        ;;
+        return 0 ;;
       11 )
         selectedEmoji=':card_file_box:'
         prettyEmoji='🗃'
-        return 0
-        ;;
+        return 0 ;;
       12 )
         selectedEmoji=':fire:'
         prettyEmoji='🔥'
-        return 0
-        ;;
+        return 0 ;;
       13 )
         selectedEmoji=':recycle:'
         prettyEmoji='♻️'
-        return 0
-        ;;
+        return 0 ;;
       14 )
         selectedEmoji=':bento:'
         prettyEmoji='🍱'
-        return 0
-        ;;
+        return 0 ;;
       15 )
         selectedEmoji=':bulb:'
         prettyEmoji='💡'
-        return 0
-        ;;
+        return 0 ;;
       16 )
         selectedEmoji=':globe_with_meridians:'
         prettyEmoji='🌐'
-        return 0
-        ;;
+        return 0 ;;
       17 )
         selectedEmoji=':see_no_evil:'
         prettyEmoji='🙈'
-        return 0
-        ;;
+        return 0 ;;
       18 )
         selectedEmoji=':alembic:'
         prettyEmoji='⚗️'
-        return 0
-        ;;
+        return 0 ;;
       19 )
         selectedEmoji=":lock:"
         prettyEmoji='🔒'
-        return 0
-        ;;
+        return 0 ;;
       20 )
         selectedEmoji=":chart_with_upwards_trend:"
         prettyEmoji='📈'
-        return 0
-        ;;
+        return 0 ;;
       21 )
         selectedEmoji=":construction_worker:"
         prettyEmoji='👷‍♂️'
-        return 0
-        ;;
+        return 0 ;;
       22 )
         selectedEmoji=":alien:"
         prettyEmoji='👽'
-        return 0
-        ;;
+        return 0 ;;
       23 )
         selectedEmoji=":ok_hand:"
         prettyEmoji='👌'
-        return 0
-        ;;
+        return 0 ;;
       24 )
         selectedEmoji=":building_construction:"
         prettyEmoji='🏗'
-        return 0
-        ;;
+        return 0 ;;
       25 )
         selectedEmoji=":rocket:"
         prettyEmoji='🚀'
-        return 0
-        ;;
+        return 0 ;;
       26 )
         selectedEmoji=":package:"
         prettyEmoji='📦'
-        return 0
-        ;;
+        return 0 ;;
+      27 )
+        selectedEmoji=":heavy_plus_sign:"
+        prettyEmoji='➕'
+        return 0 ;;
+      28 )
+        selectedEmoji=':heavy_minus_sign:'
+        prettyEmoji='➖'
+        return 0 ;;
+      29 )
+        selectedEmoji=":page_facing_up:"
+        prettyEmoji='📄'
+        return 0 ;;
       x )
         selectedEmoji=''
         prettyEmoji=''
-        return 0
-        ;;
+        return 0 ;;
       * )
         echo -e "\n $ORANGE This option doesn't exists...$YELLOW Banana!$COLOR_NONE\n"
         ;;
