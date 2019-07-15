32d5edcd5d4d6874d4ed1e596ee81b286d2705a7
Jennifer Denis <shepherd@dogstudio.be>
2019-07-15 17:26:23 +0200

    :children_crossing: Update reset and change colors

--- bin/src/display.sh
+++ bin/src/display.sh
@@ -49,6 +49,6 @@ function help()
 
 function header() {
     echo -e "\n\n  \033[37m🚀    Welcome to \033[1;33mGitRocket\033[0;37m!    🚀$COLOR_NONE"
-    echo -e "  \033[34m---------------------------------$COLOR_NONE"
+    echo -e "  ${PURPLE}---------------------------------$COLOR_NONE"
 }
 
